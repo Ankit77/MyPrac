2410426a82c5c4b65fee4d0d5a6586b82d0c734f
Ankit Khatri <ankit.khatri@indianic.com>
2015-12-31 12:57:09 +0530

    Test Commit3

--- app/src/main/java/project/ndk/com/mygittest/MainActivity.java
+++ app/src/main/java/project/ndk/com/mygittest/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //Hello Friends this is branc demo
+        //Hello Friends this is branc demo testing
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
